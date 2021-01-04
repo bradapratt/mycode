2a4967079362cded90a59270460cbbf0b5a0d18f
Brad Pratt <bradapratt@gmail.com>
2021-01-04 21:55:17 +0000

    Completed challenge #2

---
+++ challenge2.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+icecream= ["flavors", "salty"]
+icecream.append(99)
+name = input("What's your name? ")
+print(f"{str(icecream[2])} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
