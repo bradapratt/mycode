6bad51c56130f2548b0c47c1cabec028d9db93f6
Brad Pratt <bradapratt@gmail.com>
2021-01-04 18:48:50 +0000

    Completed challenge #1

---
+++ challenge1.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+name = input("Please enter your name: ")
+day = input("Please enter what day of the week it currently is: ")
+print("Hello, " + name + "! Happy " + day + "!")
+
