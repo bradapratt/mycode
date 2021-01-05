d27e934c6df17086ddf0ad68145d7a09eeb11872
Brad Pratt <bradapratt@gmail.com>
2021-01-05 02:31:25 +0000

    Completed lab 10

---
+++ advlist/animal-list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# create a list called animals
+animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+# display animals
+for animal in range(len(animals)):
+    print(animals[animal], sep = "")
--- challenge2.py
+++ challenge2.py
@@ -2,4 +2,4 @@
 icecream= ["flavors", "salty"]
 icecream.append(99)
 name = input("What's your name? ")
-print(f"{str(icecream[2])} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
+print(f"{icecream[2]} {icecream[0]}, and {name} chooses to be {icecream[1]}.")
